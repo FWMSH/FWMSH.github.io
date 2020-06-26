cd8859d465f94d0786edf391f6428ede2fbd4b82
Morgan Rehnberg <forceflow1049@gmail.com>
2020-06-26 15:25:26 -0500

    Added initial support for V-DROME

---
+++ v-drome/content.js
@@ -0,0 +1,59 @@
+var nameDict_en = {
+      '1': 'Sketchfab Test',
+      'error': 'Artifact Not Found!'
+    };
+
+var imageDict_en = {
+      '1': '',
+      'error': ''
+    };
+
+var captionDict_en = {
+  '1': '',
+  'error': ''
+}
+
+var textDict_en = {
+      '1': ' This is a model of the Museum showing the position of V-DROME.',
+      'error': "Oops, an artifact with that number does't seem to exist in this gallery. Please double check that you've selected the appropriate gallery and then try a different number!"
+    };
+
+var scriptDict_en = {
+  '1': `
+  <div class="sketchfab-embed-wrapper">
+    <iframe title="A 3D model" width="100%" height="60%" src="https://sketchfab.com/models/60409efb44ac4cccb632cf12dc5459a1/embed?autostart=1&amp;ui_controls=1&amp;ui_infos=1&amp;ui_inspector=1&amp;ui_stop=1&amp;ui_watermark=1&amp;ui_watermark_link=1" frameborder="0" allow="autoplay; fullscreen; vr" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+    <p style="font-size: 13px; font-weight: normal; margin: 5px; color: #4A4A4A;">
+        <a href="https://sketchfab.com/3d-models/fwmsh-academy-of-digital-learning-60409efb44ac4cccb632cf12dc5459a1?utm_medium=embed&utm_source=website&utm_campaign=share-popup" target="_blank" style="font-weight: bold; color: #1CAAD9;">FWMSH Academy of Digital Learning</a>
+        by <a href="https://sketchfab.com/fwmsh?utm_medium=embed&utm_source=website&utm_campaign=share-popup" target="_blank" style="font-weight: bold; color: #1CAAD9;">Fort Worth Museum of Science and History</a>
+        on <a href="https://sketchfab.com?utm_medium=embed&utm_source=website&utm_campaign=share-popup" target="_blank" style="font-weight: bold; color: #1CAAD9;">Sketchfab</a>
+    </p>
+</div>
+  `
+}
+
+var audioDict_en = {
+  '1': '',
+}
+
+var nameDict_es = {
+      'error': '¡Artefacto no encontrado!'
+    };
+
+var imageDict_es = {
+      '1': '',
+      'error': ''
+    };
+
+var captionDict_es = {
+  '1': '',
+  'error': ''
+}
+
+var textDict_es = {
+      '1': '',
+      'error': "Vaya, un artefacto con ese número no parece existir en esta galería. ¡Verifique que haya seleccionado la galería apropiada y luego intente con un número diferente!"
+    };
+
+var audioDict_es = {
+  '1': ''
+}
