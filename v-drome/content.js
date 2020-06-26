8aae2b6cb0d07b1a1704d8d4ee37085628400c2d
Morgan Rehnberg <forceflow1049@gmail.com>
2020-06-26 15:45:36 -0500

    Updated V-DROME

--- v-drome/content.js
+++ v-drome/content.js
@@ -21,9 +21,9 @@ var textDict_en = {
 var scriptDict_en = {
   '1': `
   <div class="sketchfab-embed-wrapper">
-    <iframe title="A 3D model" width="100%" height="60%" src="https://sketchfab.com/models/60409efb44ac4cccb632cf12dc5459a1/embed?autostart=1&amp;ui_controls=1&amp;ui_infos=1&amp;ui_inspector=1&amp;ui_stop=1&amp;ui_watermark=1&amp;ui_watermark_link=1" frameborder="0" allow="autoplay; fullscreen; vr" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+    <iframe title="A 3D model" width="100%" height="40%" src="https://sketchfab.com/models/72ee8a1c82a842809683c0f263af08d9/embed?autostart=1&amp;ui_controls=1&amp;ui_infos=1&amp;ui_inspector=1&amp;ui_stop=1&amp;ui_watermark=1&amp;ui_watermark_link=1" frameborder="0" allow="autoplay; fullscreen; vr" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
     <p style="font-size: 13px; font-weight: normal; margin: 5px; color: #4A4A4A;">
-        <a href="https://sketchfab.com/3d-models/fwmsh-academy-of-digital-learning-60409efb44ac4cccb632cf12dc5459a1?utm_medium=embed&utm_source=website&utm_campaign=share-popup" target="_blank" style="font-weight: bold; color: #1CAAD9;">FWMSH Academy of Digital Learning</a>
+        <a href="https://sketchfab.com/3d-models/knight-72ee8a1c82a842809683c0f263af08d9?utm_medium=embed&utm_source=website&utm_campaign=share-popup" target="_blank" style="font-weight: bold; color: #1CAAD9;">Knight</a>
         by <a href="https://sketchfab.com/fwmsh?utm_medium=embed&utm_source=website&utm_campaign=share-popup" target="_blank" style="font-weight: bold; color: #1CAAD9;">Fort Worth Museum of Science and History</a>
         on <a href="https://sketchfab.com?utm_medium=embed&utm_source=website&utm_campaign=share-popup" target="_blank" style="font-weight: bold; color: #1CAAD9;">Sketchfab</a>
     </p>
