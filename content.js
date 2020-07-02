77dc4b792536d604508ab38887b5094c7075606b
Morgan Rehnberg <forceflow1049@gmail.com>
2020-07-02 11:39:54 -0500

    Updated the schedules for STEAM Lab and Science Theater

---
+++ content.js
@@ -0,0 +1,17 @@
+var steamLabActivityList_en = [
+  {'title': 'Coffee Filter Planets', 'desc': 'Design your own planet using a coffee filter.', 'day':
+  'Friday', 'startTime': '10:00 AM', 'endTime': '10:45 AM'},
+  {'title': 'Shaving Cream Planets', 'desc': 'Use shaving cream to make a planet', 'day':
+  'Friday', 'startTime': '11:45 AM', 'endTime': '12:45 PM'},
+  {'title': 'Bicycle Pump Rockets', 'desc': 'Build and launch your own rocket', 'day':
+  'Friday', 'startTime': '1:45 PM', 'endTime': '2:45 PM'},
+];
+
+var scienceTheaterActivityList_en = [
+  {'title': 'Story Time', 'desc': "Join a member of the Museum's education team for a children's story.", 'day':
+  'Friday', 'startTime': '11:00 AM', 'endTime': '11:30 AM'},
+  {'title': 'Space Artifacts from the Collection', 'desc': "Join a member of the Museum's collection staff to explore some of our amazing space artifacts.", 'day':
+  'Friday', 'startTime': '1:00 PM', 'endTime': '1:30 PM'},
+  {'title': 'Space Artifacts from the Collection', 'desc': "Join a member of the Museum's collection staff to explore some of our amazing space artifacts.", 'day':
+  'Friday', 'startTime': '3:00 PM', 'endTime': '3:30 PM'},
+];
