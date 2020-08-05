adfc48a3b77464bb6c914dc7b5a92b3623ba9668
Morgan Rehnberg <forceflow1049@gmail.com>
2020-08-05 10:31:28 -0500

    Added wheels content

---
+++ wheels/content.js
@@ -0,0 +1,85 @@
+var nameDict_en = {
+      '1': 'Ford Model T',
+      "2": "Italian Donkey Cart",
+      "3": "Soap Box Derby Car",
+      "4": "Taiwanese Pedicab",
+      'error': 'Artifact Not Found!'
+    };
+
+var thumbDict_en = {
+    '1': 'thumbs/model_t.jpg',
+    '2': 'thumbs/donkey.jpg',
+    '3': 'thumbs/soapbox.jpg',
+    '4': 'thumbs/pedicab.jpg',
+}
+
+var imageDict_en = {
+      '1': 'images/1910Ford-T.jpg',
+      '2': 'images/Cart.jpg',
+      '3': 'images/Modern_Derby_Cars.JPG',
+      '4': 'images/Rickshaws_Dhaka.jpg',
+      'error': ''
+}
+
+var captionDict_en = {
+      '1': 'This 1910 photograph from Salt Lake City shows a Model T with a second row of seating.',
+      '2': 'An Italian cart transporting a family around 1890.',
+      '3': 'The start of a modern Soap Box Derby Race. When the race begins, the carts are released and gravity takes over!',
+      '4': 'Modern pedicabs in use alongside cars in Dhak, Bangladesh.',
+      'error': ''
+}
+
+var textDict_en = {
+      '1': "In 1863, Mr. Henry Ford was born on his family's farm outside Detroit, Michigan. Early on, he showed interest in mechanics and began working at age 16 for the Michigan Car Company, eventually landing a machinist apprenticeship at another company. His interests only increased when he started learning mechanical engineering and how gasoline engines work. Ford began several models; literally \"A\" through \"S\" before landing on the most well-known \"Model T\" version produced by the Ford Motor Company from 1908 through 1927. Regarded as the first affordable car for the everyday man, it would make traveling more accessible to people, allowing them to cover a larger distance within a shorter time frame.",
+      '2': "\"Carretto Siciliano,\" or Sicilian cart, is a handmade, wooden cart that is elaborately decorated with depictions of Sicily's rich folklore. These carts were often on the smaller side, designed for a single mule or horse. They started being used in the early nineteenth century and reached the peak of their popularity by the 1920s. The carts were geared towards carrying smaller loads across Sicily's rocky terrain.<p>The craftsmanship of these carts were passed down from generation to generation and each province in Sicily has a distinct style of cart that they are known for. Using wood and metal to create the intricate details on the cart, the imagery often portrays different periods of Italian history. Even the animal would often have an elaborate harness adorned with colorful ribbons, feathers, and tassels. Nowadays, these beautiful carts are only seen during festive occasions and are no longer used for utilitarian purposes.",
+      '3': "These homemade cars were originally built from scrap pieces of wood, such as a wooden soap box—hence how they got their name. Eventually, youths started using other materials for their cars as the sport evolved. The derby's history comes out of America's Great Depression, drawing inspiration from both downhill sledding and auto racing. What began in Ohio, quickly became a nationwide racing circuit where kids could compete at a local or regional level. By the mid 1930s, those regional winners would qualify to attend the annual Derby race in Akron, Ohio.",
+      '4': 'The origins of who invented pedicabs or "rickshaws" are not completely known, but it is believed to be a Western design or concept. There are a few possible men attributed to the invention, however, these small wagons begin popping up in Asia around the mid-1800s. They become very popular as a quick form of inner-city transportation. Earlier models needed manpower, literally: runners were needed to pull the carts by hand. Later on, bicycles would be fitted to the carts, making it easier on the drivers.',
+      'error': "Oops, an artifact with that number does't seem to exist in this gallery. Please double check that you've selected the appropriate gallery and then try a different number!"
+    };
+
+var audioDict_en = {
+      '1': 'audio/en/model-t.mp3',
+      '2': 'audio/en/donkey-cart.mp3',
+      '3': 'audio/en/soapbox.mp3',
+      '4': 'audio/en/pedicab.mp3',
+}
+
+var nameDict_es = {
+      '1': '',
+      '2': '',
+      '3': '',
+      '4': '',
+      'error': ''
+    };
+
+var imageDict_es = {
+      '1': 'images/Mohenjo-daro.jpg',
+      '2': 'images/Spiro_Craig_Mound_HRoe.jpg',
+      '3': 'images/Massai_warriors.jpg',
+      '4': 'images/Londons_Carnaby_Street,_1966.jpg',
+      'error': ''
+}
+
+var captionDict_es = {
+      '1': '',
+      '2': '',
+      '3': '',
+      '4': '',
+      'error': ''
+}
+
+var textDict_es = {
+      '1': '',
+      '2': '',
+      '3': '',
+      '4': '',
+      'error': "Vaya, un artefacto con ese número no parece existir en esta galería. ¡Verifique que haya seleccionado la galería apropiada y luego intente con un número diferente!"
+    };
+
+var audioDict_es = {
+      '1': '',
+      '2': '',
+      '3': '',
+      '4': '',
+
+}
