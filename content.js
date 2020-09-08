da7e051a6a6927bc175cc64436619384893f7d8e
Morgan Rehnberg <forceflow1049@gmail.com>
2020-09-08 09:24:01 -0500

    Update schedule

--- content.js
+++ content.js
@@ -8,6 +8,15 @@ var steamLabActivityList_en = [
 {'name': 'Weathering Erosion and Deposition', 'desc': '', 'date': '9/5/2020', 'startTime': '10:00 AM', 'endTime': '10:45 AM'},
 {'name': 'Camping Tent STEM Challenge', 'desc': '', 'date': '9/5/2020', 'startTime': '11:30 AM', 'endTime': '12:00 PM'},
 {'name': 'Colors in Nature - Cochineal Painting', 'desc': '', 'date': '9/5/2020', 'startTime': '1:30 PM', 'endTime': '2:30 PM'},
+{'name': 'Foil Boat STEAM Challenge', 'desc': '', 'date': '9/10/2020', 'startTime': '10:00 AM', 'endTime': '10:45 AM'},
+{'name': 'Mixed Media Jellyfish', 'desc': '', 'date': '9/10/2020', 'startTime': '11:30 AM', 'endTime': '12:00 PM'},
+{'name': 'Foil Boat STEAM Challenge', 'desc': '', 'date': '9/10/2020', 'startTime': '1:30 PM', 'endTime': '2:30 PM'},
+{'name': 'Foil Boat STEAM Challenge', 'desc': '', 'date': '9/11/2020', 'startTime': '10:00 AM', 'endTime': '10:45 AM'},
+{'name': 'Mixed Media Jellyfish', 'desc': '', 'date': '9/11/2020', 'startTime': '11:30 AM', 'endTime': '12:00 PM'},
+{'name': 'Foil Boat STEAM Challenge', 'desc': '', 'date': '9/11/2020', 'startTime': '1:30 PM', 'endTime': '2:30 PM'},
+{'name': 'Foil Boat STEAM Challenge', 'desc': '', 'date': '9/12/2020', 'startTime': '10:00 AM', 'endTime': '10:45 AM'},
+{'name': 'Mixed Media Jellyfish', 'desc': '', 'date': '9/12/2020', 'startTime': '11:30 AM', 'endTime': '12:00 PM'},
+{'name': 'Foil Boat STEAM Challenge', 'desc': '', 'date': '9/12/2020', 'startTime': '1:30 PM', 'endTime': '2:30 PM'},
 ];
 
 var scienceTheaterActivityList_en = [
@@ -20,4 +29,13 @@ var scienceTheaterActivityList_en = [
 {'name': 'Story Time with Animals – Around One Cactus', 'desc': '', 'date': '9/5/2020', 'startTime': '11:00 AM', 'endTime': '11:20 AM'},
 {'name': 'Animal Talk', 'desc': '', 'date': '9/5/2020', 'startTime': '12:00 PM', 'endTime': '12:30 PM'},
 {'name': 'Story Time with Animals – Around One Cactus', 'desc': '', 'date': '9/5/2020', 'startTime': '3:00 PM', 'endTime': '3:30 PM'},
+{'name': 'Story Time with Animals - A Day at Seagull Beach', 'desc': '', 'date': '9/10/2020', 'startTime': '11:00 AM', 'endTime': '11:20 AM'},
+{'name': 'Story Time with Animals - A House for Hermit Crab', 'desc': '', 'date': '9/10/2020', 'startTime': '12:00 PM', 'endTime': '12:30 PM'},
+{'name': 'Animal Talk - Texas Seashore Wildlife', 'desc': '', 'date': '9/10/2020', 'startTime': '3:00 PM', 'endTime': '3:30 PM'},
+{'name': 'Story Time with Animals - A Day at Seagull Beach', 'desc': '', 'date': '9/11/2020', 'startTime': '11:00 AM', 'endTime': '11:20 AM'},
+{'name': 'Story Time with Animals - A House for Hermit Crab', 'desc': '', 'date': '9/11/2020', 'startTime': '12:00 PM', 'endTime': '12:30 PM'},
+{'name': 'Story Time with Animals - A Day at Seagull Beach', 'desc': '', 'date': '9/11/2020', 'startTime': '3:00 PM', 'endTime': '3:30 PM'},
+{'name': 'Story Time with Animals - A Day at Seagull Beach', 'desc': '', 'date': '9/12/2020', 'startTime': '11:00 AM', 'endTime': '11:20 AM'},
+{'name': 'Story Time with Animals - A House for Hermit Crab', 'desc': '', 'date': '9/12/2020', 'startTime': '12:00 PM', 'endTime': '12:30 PM'},
+{'name': 'Story Time with Animals - A Day at Seagull Beach', 'desc': '', 'date': '9/12/2020', 'startTime': '3:00 PM', 'endTime': '3:30 PM'},
 ];
