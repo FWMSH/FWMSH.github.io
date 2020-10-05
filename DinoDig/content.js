98b47e96a37e05b0950408b902e812d0ebc001a1
Morgan Rehnberg <forceflow1049@gmail.com>
2020-10-05 16:16:05 -0500

    Added new section for DinoDig and fixed a bug involving font size inheritance

---
+++ DinoDig/content.js
@@ -0,0 +1,94 @@
+var nameDict_en = {
+      '1': '<i>Deinonychus</i>',
+      "2": "How Do We Know<br>How Old Our Site Is?",
+      "3": "<i>Paluxysaurus</i> Bones",
+      "4": "Field Jacket",
+      "5": "Crossbedding",
+      'error': 'Artifact Not Found!'
+    };
+
+var thumbDict_en = {
+    '1': 'thumbs/station-1.jpg',
+    '2': 'thumbs/station-2.jpg',
+    '3': 'thumbs/station-3.jpg',
+    '4': 'thumbs/station-4.jpg',
+    '5': 'thumbs/station-5.jpg'
+}
+
+var imageDict_en = {
+      '1': 'images/1-Deinonychus.jpg',
+      '2': 'images/2-Ammonite.jpg',
+      '3': '',
+      '4': '',
+      '5': "images/5-Crossbedding.jpg",
+      'error': ''
+}
+
+var captionDict_en = {
+      '1': 'A skeleton of <i>Deinonychus</i> on display at Chicago\'s Field Museum of Natural History (image credit: Jonathan Chen )',
+      '2': 'A fossilized ammonite. Four ammonite fossils can be seen in the Museum\'s DinoLabs gallery.',
+      '3': "",
+      '4': "",
+      '5': "Crossbedding visible in Utah's Zion Canyon.",
+      'error': ''
+}
+
+var textDict_en = {
+      '1': "Here is a complete skeleton of <i>Deinonychus</i>This meat-eating dinosaur skeleton shows the effects of bleaching and weathering. It is in a relatively intact position, with the head and neck twisted backwards, suggesting it dried out enough for the tendons to shrink and contort the head and neck before it was covered and preserved from further damage.",
+
+      '2': "So how old is our site? Fossilized ammonites can be found in the marine limestone lying just above the Cretaceous strata in the quarry wall. They can be used as index fossils to determine that our site is around 112 million years old.\nAn <b>index fossil</b> is a fossil that is known to have lived during a particular geologic time interval and is used to date the rock layer(s) in which it is found. Typically, good index fossils are from animals that lived for a very brief time period and were geographically widespread.",
+
+      '3': "The upper sandstone unit at our site contains both scattered and articulated remains of a large plant-eating dinosaur <i>Paluxysaurus</i>. The bones aren't all connected to other skeletal elements like the <i>Deinonychus</i> at Station 1.This is the way paleontologists typically find dinosaur bones.",
+
+      '4': "Field jackets containing fossils from the site are transported to a laboratory for further preparation. Paleontologists carefully cut open the jacket, remove the fossils, and study them in greater detail.",
+
+      '5': "A slow-moving river once ran through our site and made this pattern by depositing sand and mud. Cross-bedding occurs when sediment is deposited on rock as water moves across it from a single direction. The sediment grains build up into piles until the reach an unstable height. The grains then avalanche down and make thin layers. Since the layers are formed in this manner, they provide us information on the direction of flow for this prehistoric stream."
+    };
+
+var audioDict_en = {
+      '1': 'audio/en/',
+      '2': 'audio/en/',
+      '3': 'audio/en/',
+      '4': 'audio/en/',
+      '5': 'audtio/en/',
+}
+
+var nameDict_es = {
+      '1': '',
+      '2': '',
+      '3': '',
+      '4': '',
+      'error': ''
+    };
+
+var imageDict_es = {
+      '1': 'images/Mohenjo-daro.jpg',
+      '2': 'images/Spiro_Craig_Mound_HRoe.jpg',
+      '3': 'images/Massai_warriors.jpg',
+      '4': 'images/Londons_Carnaby_Street,_1966.jpg',
+      'error': ''
+}
+
+var captionDict_es = {
+      '1': '',
+      '2': '',
+      '3': '',
+      '4': '',
+      'error': ''
+}
+
+var textDict_es = {
+      '1': '',
+      '2': '',
+      '3': '',
+      '4': '',
+      'error': "Vaya, un artefacto con ese número no parece existir en esta galería. ¡Verifique que haya seleccionado la galería apropiada y luego intente con un número diferente!"
+    };
+
+var audioDict_es = {
+      '1': '',
+      '2': '',
+      '3': '',
+      '4': '',
+
+}
