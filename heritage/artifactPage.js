321008cc8ede494c313e550abfb054c4573ecc95
Morgan Rehnberg <forceflow1049@gmail.com>
2020-11-03 17:14:50 -0600

    Less broken UI test

--- heritage/artifactPage.js
+++ heritage/artifactPage.js
@@ -149,7 +149,6 @@ function buildPage(num) {
 
     document.getElementById('header').style.display = 'none';
     document.getElementById('inputPane').style.display = 'none';
-    document.getElementById('optionButtonPane').style.display = 'none'
     document.getElementById('contentPane').style.display = 'block';
 
   }
